2d0467da918466d9593d957d6713afa446786967
skzoh <setproof@yandex.ru>
2020-05-16 15:01:46 +0300

    Произведжена компиляция и запуск на другой машине win64.

--- deployment_test/srdt_MinGW_32bit-Debug/start.bat
+++ deployment_test/srdt_MinGW_32bit-Debug/start.bat
@@ -1,5 +1,6 @@
 
-@srdt.exe  --xmlpath=D:\Dokuments\Kuzmenko\Programs\PROGRAM_PROJECTS\srdtrep\xmltestprj\prj1\
+::@srdt.exe  --xmlpath=D:\Dokuments\Kuzmenko\Programs\PROGRAM_PROJECTS\srdtrep\xmltestprj\prj1\
+@srdt.exe  --xmlpath=..\..\xmltestprj\prj1\
 
 
 
