948581625c4b0b7c26d3f510efbd70b06c2420b3
skzow <setproof@yandex.ru>
2020-09-09 16:44:57 +0300

    Промежуточное сохранение.

--- basicwidgets/widgets/lcqremcombobox.cpp
+++ basicwidgets/widgets/lcqremcombobox.cpp
@@ -1,16 +1,22 @@
 ﻿#include "lcqremcombobox.h"
+#include "LIRemoteDataReader.h"
+#include "LIRemoteDataWriter.h"
 #include <QCoreApplication>
 #include <QDebug>
 #include <qcombobox.h>
+#include <qglobal.h>
 
 //=========================================================================================================CReadListener
 LCQRemComboBox::CReadListener::CReadListener(LCQRemComboBox& _label) : mComboBox(_label)
 {
 
 }
-//------------------------------------------------------------------------------------------------------------dataIsRead
+
+//---------------------------------------------------------------------------------------------------------------------
 void LCQRemComboBox::CReadListener::dataIsRead(QSharedPointer<QByteArray> _data, LERemoteDataStatus _status)
 {
+    Q_UNUSED(_data);
+    Q_UNUSED(_status);
     qDebug() << "LCQRemComboBox " << mComboBox.mDataName << ": data is read";
 }
 
@@ -20,12 +26,11 @@ LCQRemComboBox::LCQRemComboBox(QWidget* _parent) : QComboBox(_parent)
 {
 }
 
-LCQRemComboBox::LCQRemComboBox(const QString& _dataName,
-                         QSharedPointer<LIRemoteDataSource> _dataSource,
-                         QSharedPointer<LCStringDataFormatterBase> _formatter,
-                         QWidget* _parent) :    QComboBox(_parent),
-                                                mDataName(_dataName),
-                                                mFormatter(_formatter)
+LCQRemComboBox::LCQRemComboBox(  const QString&                               _dataName,
+                                 QSharedPointer<LIRemoteDataSource>           _dataSource,
+                                 QSharedPointer<LCStringDataFormatterBase>    _formatter,
+                                 QWidget* _parent) :    QComboBox(_parent),
+                                                        mFormatter(_formatter)
 {
     this->insertItem(0, mFormatter.data()->undefStateString());
     mDataListener = QSharedPointer<CReadListener>(new CReadListener(*this));
@@ -33,28 +38,35 @@ LCQRemComboBox::LCQRemComboBox(const QString& _dataName,
     mDataReader->setDataName(_dataName);
     mDataReader->setDataSource(_dataSource);
     mDataReader->setDataReadListener(mDataListener);
-
+    mDataWriter->setDataName(_dataName);
+    mDataWriter->setDataSource(_dataSource);
 }
 
+LCQRemComboBox::LCQRemComboBox( const QString&                              _dataNameRead,
+                                const QString&                              _dataNameWrite,
+                                QSharedPointer<LIRemoteDataSource>          _dataSource,
+                                QSharedPointer<LCStringDataFormatterBase>   _formatter,
+                                QWidget* _parent)
+{
+    this->insertItem(0, mFormatter.data()->undefStateString());
+    mDataListener = QSharedPointer<CReadListener>(new CReadListener(*this));
+
+    mDataReader = _dataSource->createReader();
+    mDataReader->setDataName(_dataNameRead);
+    mDataReader->setDataSource(_dataSource);
+
+    mDataReader->setDataReadListener(mDataListener);
+    mDataWriter->setDataName(_dataNameWrite);
+    mDataWriter->setDataSource(_dataSource);
+}
+
+//---------------------------------------------------------------------------------------------------------------------
 LCQRemComboBox::~LCQRemComboBox()
 {
     qDebug() << "LCQRemComboBox::~LCQRemComboBox()";
 }
 
-//-------------------------------------------------------------------------------------------------------------setActive
-void LCQRemComboBox::setActive(bool _flag)
-{
-    if(_flag)
-    {
-        mDataReader->connectToSource();
-    }
-    else
-    {
-        mDataReader->disconnectFromSource();
-    }
-}
-
-//-----------------------------------------------------------------------------------------------------------------event
+//---------------------------------------------------------------------------------------------------------------------
 bool LCQRemComboBox::event(QEvent *_event)
 {
     bool ret = false;
