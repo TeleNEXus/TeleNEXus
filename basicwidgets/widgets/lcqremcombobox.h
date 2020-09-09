e0beabfa15314043ac87eaf48070701490d66cdb
SergeyKuzmenko <serkuzmo@gmail.com>
2020-09-09 16:44:57 +0300

    Промежуточное сохранение.

--- basicwidgets/widgets/lcqremcombobox.h
+++ basicwidgets/widgets/lcqremcombobox.h
@@ -2,6 +2,7 @@
 #define LCQEXTLABEL_H
 
 #include <QComboBox>
+#include "LIRemoteDataWriter.h"
 #include "lqextendevent.h"
 #include "lcstringdataformatterbase.h"
 
@@ -13,7 +14,8 @@ class LCQRemComboBox : public QComboBox
 {
     Q_OBJECT
 private:
-
+    
+    //-----------------------------------------------------------------------------------------------
     class CReadListener : public LIRemoteDataReadListener
     {
     private:
@@ -23,7 +25,7 @@ private:
         virtual ~CReadListener(){}
         virtual void dataIsRead(QSharedPointer<QByteArray> _data, LERemoteDataStatus status) override;
     };
-
+    //-----------------------------------------------------------------------------------------------
     class CWriteListener : public LIRemoteDataWriteListner
     {
     public:
@@ -32,27 +34,26 @@ private:
         virtual void dataIsWrite(LERemoteDataStatus _status) = 0;
     };
 
-    QString mDataName;
-    QSharedPointer<LIRemoteDataReader>  mDataReader;
-    QSharedPointer<LCStringDataFormatterBase> mFormatter;
     QSharedPointer<CReadListener> mDataListener;
+    QSharedPointer<LIRemoteDataWriter>  mDataWriter;
+    QSharedPointer<LCStringDataFormatterBase> mFormatter;
 
 public:
     explicit LCQRemComboBox(QWidget* _parent = nullptr);
 
     explicit LCQRemComboBox(QString _text, QWidget* _parent = nullptr);
 
-    explicit LCQRemComboBox(   const QString&                              _dataName,
+    explicit LCQRemComboBox(   const QString&                           _dataName,
                             QSharedPointer<LIRemoteDataSource>          _dataSource,
                             QSharedPointer<LCStringDataFormatterBase>   _formatter,
                             QWidget* _parent = nullptr);
+
+    explicit LCQRemComboBox(    const QString&                               _dataNameRead,
+                                const qstring&                               _dataNameWrite,
+                                QSharedPointer<LIRemoteDataSource>           _dataSource,
+                                QSharedPointer<LCStringDataFormatterBase>    _formatter,
+                                QWidget* _parent = nullptr);
     virtual ~LCQRemComboBox();
-
-    void setActive(bool _flag);
-
-//    void setSource( const QString& _dataName,
-//                    QWeakPointer<LCRemoteDataSourceInterface>   _dataSource,
-//                    QSharedPointer<LCStringDataFormatterBase>         _formatter);
 
     virtual bool event(QEvent *e) override;
 
--- testqt/testComboBox/testcombobox.pro
+++ testqt/testComboBox/testcombobox.pro
@@ -7,7 +7,6 @@ CONFIG -= app_bundle
 
 # The following define makes your compiler emit warnings if you use
 # any Qt feature that has been marked deprecated (the exact warnings
-# depend on your compiler). Please consult the documentation of the
 # deprecated API in order to know how to port your code away from it.
 DEFINES += QT_DEPRECATED_WARNINGS
 
@@ -16,7 +15,6 @@ DEFINES += QT_DEPRECATED_WARNINGS
 # You can also select to disable deprecated APIs only up to a certain version of Qt.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
-INCLUDEPATH +=                                  \
     
 
 SOURCES += main.cpp \
