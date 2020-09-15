0ca2676f6991fdbcf56f09362bfa62b1997b24e1
SergeyKuzmenko <serkuzmo@gmail.com>
2020-09-15 10:56:33 +0300

    Добавлен контроль на ненулевой указатель слушателя события окончания записи.

--- modbussource/modbus/lqmodbusdatawriter.cpp
+++ modbussource/modbus/lqmodbusdatawriter.cpp
@@ -5,29 +5,30 @@
 namespace modbus
 {
 
-//======================================================================================================================
+//==============================================================================
 __LQ_EXTENDED_QEVENT_IMPLEMENTATION(LQModbusDataWriter::CQEventDataIsWrite);
 
 LQModbusDataWriter::
         CQEventDataIsWrite::
-            CQEventDataIsWrite(LERemoteDataStatus _status) : QEvent(__LQ_EXTENDED_QEVENT_REGISTERED),
-                                                            mStatus(_status)
+            CQEventDataIsWrite(LERemoteDataStatus _status) : 
+                                QEvent(__LQ_EXTENDED_QEVENT_REGISTERED),
+                                mStatus(_status)
 {
 }
 
-//======================================================================================================================
+//==============================================================================
 LQModbusDataWriter::LQModbusDataWriter(QObject *_parent) : QObject(_parent)
 {
 
 }
 
-//----------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 void LQModbusDataWriter::setDataName(const QString& _dataName)
 {
     mDataName = _dataName;
 }
 
-//----------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 void LQModbusDataWriter::setDataSource(QWeakPointer<LIRemoteDataSource> _source)
 {
     QSharedPointer<LIRemoteDataSource> sp = _source.lock();
@@ -37,21 +38,26 @@ void LQModbusDataWriter::setDataSource(QWeakPointer<LIRemoteDataSource> _source)
     }
 }
 
-//----------------------------------------------------------------------------------------------------------------------
-void LQModbusDataWriter::setDataWriteListener(QWeakPointer<LIRemoteDataWriteListner> _listener)
+//------------------------------------------------------------------------------
+void LQModbusDataWriter::
+        setDataWriteListener(QWeakPointer<LIRemoteDataWriteListner> _listener)
 {
     QSharedPointer<LIRemoteDataWriteListner> sp = _listener.lock();
     if(!sp.isNull()) mwpWriteListener = sp;
 }
 
-//----------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 void LQModbusDataWriter::writeRequest(const QByteArray& _data)
 {
     QSharedPointer<LIRemoteDataSource> sp = mDataSource.lock();
     if(sp.isNull())
     {
-        QSharedPointer<LIRemoteDataWriteListner> listener = mwpWriteListener.lock();
-        listener->dataIsWrite(LERemoteDataStatus::DS_WRONG);
+        QSharedPointer<LIRemoteDataWriteListner> listener = 
+                                                        mwpWriteListener.lock();
+        if(!listener.isNull())
+        {
+            listener->dataIsWrite(LERemoteDataStatus::DS_WRONG);
+        }
     }
     else
     {
@@ -59,20 +65,24 @@ void LQModbusDataWriter::writeRequest(const QByteArray& _data)
     }
 }
 
-//----------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 void LQModbusDataWriter::customEvent(QEvent *_event)
 {
     if(_event->type() == CQEventDataIsWrite::msExtendedEventType)
     {
-        QSharedPointer<LIRemoteDataWriteListner> listener = mwpWriteListener.lock();
-        CQEventDataIsWrite *e = dynamic_cast<CQEventDataIsWrite*>(_event);
-        if(e != nullptr)
+        QSharedPointer<LIRemoteDataWriteListner> listener = 
+                                                        mwpWriteListener.lock();
+        if(!listener.isNull())
         {
-            listener->dataIsWrite(LERemoteDataStatus::DS_WRONG);
-        }
-        else
-        {
-            listener->dataIsWrite(e->mStatus);
+            CQEventDataIsWrite *e = dynamic_cast<CQEventDataIsWrite*>(_event);
+            if(e != nullptr)
+            {
+                listener->dataIsWrite(LERemoteDataStatus::DS_WRONG);
+            }
+            else
+            {
+                listener->dataIsWrite(e->mStatus);
+            }
         }
     }
 }
