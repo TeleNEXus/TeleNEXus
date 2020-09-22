acb2b65494c07d944e63d343cf576e7cbe7a54f1
SergeyKuzmenko <serkuzmo@gmail.com>
2020-09-22 08:18:17 +0300

    Добавлены файлы нового тестового конфигурационного набора.

---
+++ xmltestprj/linux/prj1/launch.sh
@@ -0,0 +1,2 @@
+cd ../../../srdt/__builds/
+./srdt  --xmlpath=../../xmltestprj/linux/prj1
---
+++ xmltestprj/linux/prj1/libs/create_links.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -s ../../../../basiclayouts/__builds/libbasiclayouts.so
+ln -s ../../../../basicwidgets/__builds/libbasicwidgets.so
+ln -s ../../../../modbussource/__builds/libmodbussource.so
+
