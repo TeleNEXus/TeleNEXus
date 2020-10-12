14eb6479b9c5baa861979c8bccfcb7dd6c83bd0c
skzow <setproof@yandex.ru>
2020-10-12 17:05:23 +0300

    Добавлена возможность загузки виджета из файда

--- basiclayouts/basiclayouts.cpp
+++ basiclayouts/basiclayouts.cpp
@@ -3,25 +3,31 @@
 #include "lcxmlboxlayoutbuilder.h"
 
 
-//----------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 void* BASICLAYOUTS_EXPORT getGridLayoutBuilder()
 {
     return new LCXmlGridLayoutBuilder;
 }
 
-//----------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 void* BASICLAYOUTS_EXPORT getBoxLayoutBuilder()
 {
     return new LCXmlBoxLayoutBuilder;
 }
 
-////----------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+void* BASICLAYOUTS_EXPORT getLayoutBuilder()
+{
+    return new LCXmlBoxLayoutBuilder;
+}
+
+////----------------------------------------------------------------------------
 //void* BASICLAYOUTS_EXPORT getVBoxLayoutBulder()
 //{
 //    return new LCXmlBoxLayoutBuilder(QBoxLayout::Direction::TopToBottom);
 //}
 
-////----------------------------------------------------------------------------------------------------------------------
+////----------------------------------------------------------------------------
 //void* BASICLAYOUTS_EXPORT getHBoxLayoutBulder()
 //{
 //    return new LCXmlBoxLayoutBuilder(QBoxLayout::Direction::LeftToRight);
--- basicwidgets/builders/lcxmlwidgetbuilder.cpp
+++ basicwidgets/builders/lcxmlwidgetbuilder.cpp
@@ -3,19 +3,22 @@
 #include "LIXmlLayoutBuilder.h"
 #include <QWidget>
 #include <QDomElement>
-
+#include <QFile>
 #include <QDebug>
 
+//------------------------------------------------------------------------------
 static const struct
 {
     QString layout = "layout";
-} sl_Tags;
+} __tagsName;
 
+//------------------------------------------------------------------------------
 static const struct
 {
+    QString file = "file";
     QString title = "title";
     QString type  = "type";
-} sl_Attr;
+} __attrName;
 
 //==============================================================================
 LCXmlWidgetBuilder::LCXmlWidgetBuilder()
@@ -28,25 +31,54 @@ LCXmlWidgetBuilder::~LCXmlWidgetBuilder()
 {
 
 }
+//------------------------------------------------------------------------------
+static QWidget* buildWidget(
+        const QDomElement& _element, 
+        const LIApplication& _app);
+static QDomElement loadDomElement(const QString& _fileName);
 
 //------------------------------------------------------------------------------
 QWidget* LCXmlWidgetBuilder::build( const QDomElement& _element, 
                                     const LIApplication& _app)
 {
+
+    QString attr;
+
+    attr = _element.attribute(__attrName.file);
+
+    if(!attr.isNull())
+    {
+        QString file = _app.getProjectPath() + attr;
+        QDomElement el = loadDomElement(file);
+        if(!el.isNull())
+        {
+            if(el.tagName() == _element.tagName()) return buildWidget(el, _app);
+        } 
+    }
+
+    return buildWidget(_element, _app);
+}
+
+//------------------------------------------------------------------------------
+static QWidget* buildWidget(
+        const QDomElement& _element, 
+        const LIApplication& _app)
+{
     QWidget* widget = new QWidget;
 
     QString attr;
 
-    attr = _element.attribute(sl_Attr.title);
+    attr = _element.attribute(__attrName.title);
 
     if(!attr.isNull()) widget->setWindowTitle(attr);
 
-    QDomElement layout = _element.elementsByTagName(
-                                            sl_Tags.layout).at(0).toElement();
+    QDomElement layout = 
+        _element.elementsByTagName(
+                __tagsName.layout).at(0).toElement();
 
     if(!layout.isNull())
     {
-        QString type = layout.attribute(sl_Attr.type);
+        QString type = layout.attribute(__attrName.type);
         if(!type.isNull())
         {
             auto builder = _app.getLayoutBuilder(type);
@@ -56,7 +88,26 @@ QWidget* LCXmlWidgetBuilder::build( const QDomElement& _element,
             }
         }
     }
-
     return widget;
 }
 
+//==============================================================================
+static QDomElement loadDomElement(const QString& _fileName)
+{
+    QFile file(_fileName);
+
+    QDomDocument domDoc;
+    QString errorStr;
+    int errorLine;
+    int errorColumn;
+
+    if(!domDoc.setContent(&file, true, &errorStr, &errorLine, &errorColumn))
+    {
+        qDebug() << 
+            "Parse file "       << file.fileName() << 
+            " error at line:"   << errorLine <<
+            " column:"          << errorColumn << 
+            " msg: "            << errorStr;
+    }
+    return domDoc.documentElement();
+}
