b257ff8741bb006cbc7b6dbd2611ed1d2ad2fd1c
SergeyKuzmenko <serkuzmo@gmail.com>
2020-10-16 08:37:42 +0300

    Добавлены файлы общих данных построителей.

---
+++ basiclayouts/builders/common.h
@@ -0,0 +1,32 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+/*
+ * Общие данные и функции для всех построителей.
+ */
+#include <QString>
+#include <qnamespace.h>
+
+struct CCommonAttributes
+{
+public:
+    struct SAligns{
+        const QString attrName = "align";
+        const struct
+        {
+            QString Left    = "Left";
+            QString Right   = "Right";
+            QString Center  = "Center";
+            QString HCenter = "HCenter";
+            QString Top     = "Top";
+            QString Bottom  = "Bottom";
+            QString VCenter = "VCenter";
+        }vals;
+        Qt::AlignmentFlag toFlags(const QString& _attributes);
+    };
+    SAligns mAligns;
+private:
+    CCommonAttributes();
+public:
+    static CCommonAttributes& instance();
+};
+#endif
