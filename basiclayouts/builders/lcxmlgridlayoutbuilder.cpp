bdebfaed68a2824e23996d941b34b31d5bd6e8c4
SergeyKuzmenko <serkuzmo@gmail.com>
2020-10-16 16:10:17 +0300

    Проведено предваритлеьное тестирование добавления компроновщика QGridLayout.

--- basiclayouts/builders/lcxmlgridlayoutbuilder.cpp
+++ basiclayouts/builders/lcxmlgridlayoutbuilder.cpp
@@ -17,6 +17,7 @@ static const struct
     QString hspacing    = "hspacing";
     QString vspacing    = "vspacing";
     QString value       = "value";
+    QString minheight   = "minheight";
     QString minwidth    = "minwidth";
 }__sAttributes;
 
@@ -38,8 +39,10 @@ public:
     int mRow = 0;
     int mColumn = 0;
     QGridLayout* mpLayout = nullptr;
+
     //Карта глобального выравнивания элементов столбцов.
     QMap<int, Qt::AlignmentFlag> mColumnAlign; 
+
     //Карта глобального выравнивания элементов строк.
     QMap<int, Qt::AlignmentFlag> mRowAlign;
 private:
@@ -71,11 +74,11 @@ static void buildCol(
         const LIApplication& _app, 
         CBuildData& _buildData);
 
+//------------------------------------------------------------------------------build
 QLayout* LCXmlGridLayoutBuilder::build(
         const QDomElement& _element, 
         const LIApplication& _app)
 {
-    qDebug() << "QGridLayout build 0";
     QGridLayout* layout = new QGridLayout;
     CBuildData buildData(layout);
 
@@ -91,7 +94,9 @@ QLayout* LCXmlGridLayoutBuilder::build(
         }
     }
 
-    for(QDomNode node = _element.firstChild(); !node.isNull(); node = node.nextSiblingElement())
+    for(    QDomNode node = _element.firstChild(); 
+            !node.isNull(); 
+            node = node.nextSiblingElement())
     {
         if(!node.isElement()) continue;
         QDomElement el = node.toElement();
@@ -108,7 +113,7 @@ QLayout* LCXmlGridLayoutBuilder::build(
     return layout;
 }
 
-//==============================================================================
+//==============================================================================buildRow
 static int addRowWidgets(
         const QDomElement &_element,
         CBuildData& _buildData, 
@@ -134,6 +139,20 @@ static void buildRow(
 {
     int curr_col = 0;
 
+    //Определение минимально заданной высоны строки.
+    QString attr_minheight =
+        _element.attribute(__sAttributes.minheight);
+
+    if(!attr_minheight.isNull())
+    {
+        bool flag = false;
+        int minheight = attr_minheight.toInt(&flag);
+        if(flag)
+        {
+            _buildData.mpLayout->setRowMinimumHeight(
+                    _buildData.mRow, minheight);
+        }
+    }
     //Анализ глобального выравнивания для элементов строки.
     QString attr_align = 
         _element.attribute(CCommonAttributes::instance().mAligns.attrName);
@@ -170,14 +189,14 @@ static void buildRow(
     _buildData.mRow++;
 }
 
-//==============================================================================
+//==============================================================================addRowWidgets
 static int addRowWidgets(
         const QDomElement &_element,
         CBuildData &_buildData, 
         int _startCol, 
         const LIApplication& _app)
 {
-    int col = _startCol;
+    int col = 0;
 
     quint16 align_flags = 0; 
     //Анализ атрибута выравнивания элемента.
@@ -223,14 +242,14 @@ static int addRowWidgets(
         _buildData.mpLayout->addWidget( 
                 widget, 
                 _buildData.mRow, 
-                col, 
+                col + _startCol, 
                 static_cast<Qt::AlignmentFlag>(align_flags)); 
         col++;
     }
     return col;
 }
 
-//==============================================================================
+//==============================================================================addRowLayout
 static int addRowLayout(
         const QDomElement &_element,
         CBuildData &_buildData, 
@@ -253,7 +272,7 @@ static int addRowLayout(
     return 0; 
 }
 
-//==============================================================================
+//==============================================================================addRowStretch
 static int addRowStretch(
         const QDomElement &_element,
         CBuildData &_buildData, 
@@ -275,91 +294,185 @@ static int addRowStretch(
     return 0;
 }
 
+//==============================================================================buildCol
+static int addColWidgets(
+        const QDomElement &_element,
+        CBuildData& _buildData, 
+        int _startRow, 
+        const LIApplication& _app);
 
+static int addColLayout(
+        const QDomElement &_element,
+        CBuildData& _buildData, 
+        int _startRow, 
+        const LIApplication& _app);
 
+static int addColStretch(
+        const QDomElement &_element,
+        CBuildData& _buildData, 
+        int _startRow);
 
-//==============================================================================
+//------------------------------------------------------------------------------
 static void buildCol(
         const QDomElement &_element, 
         const LIApplication& _app, 
         CBuildData& _buildData)
 {
-    Q_UNUSED(_element);
-    Q_UNUSED(_app);
-    Q_UNUSED(_buildData);
-    /* quint32 row = 0; */
+    int curr_row = 0;
 
-    /* QDomNode childNode = _element.firstChild(); */
+    //Определение минимально заданной высоны строки.
+    QString attr_minwidth =
+        _element.attribute(__sAttributes.minwidth);
 
-    /* if(childNode.isNull()) */
-    /* { */
-    /*     _buildData.mpLayout-> */
-    /*         addItem(new QSpacerItem(0,0), row, _buildData.mColumn); */
+    if(!attr_minwidth.isNull())
+    {
+        bool flag = false;
+        int minwidth = attr_minwidth.toInt(&flag);
+        if(flag)
+        {
+            _buildData.mpLayout->setColumnMinimumWidth(
+                    _buildData.mColumn, minwidth);
+        }
+    }
 
-    /*     _buildData.mColumn++; */
-    /*     return; */
-    /* } */
+    //Анализ глобального выравнивания для элементов строки.
+    QString attr_align = 
+        _element.attribute(CCommonAttributes::instance().mAligns.attrName);
 
-    /* while(!childNode.isNull()) */
-    /* { */
-    /*     if(childNode.isElement()) */
-    /*     { */
-    /*         QDomElement nodeElement = childNode.toElement(); */
-    /*         if(nodeElement.tagName() == */ 
-    /*                 LCXmlLayoutBuilderBase::mCommonTags.layout) */
-    /*         { */
-    /*             QLayout* layout = nullptr; */
+    if(!attr_align.isNull())
+    {
+        _buildData.mColumnAlign[_buildData.mRow] = 
+            CCommonAttributes::instance().mAligns.toFlags(attr_align);
+    }
 
-    /*             QSharedPointer<LIXmlLayoutBuilder> */ 
-    /*                 builder = */
-    /*                 _app.getLayoutBuilder(nodeElement.attribute( */
-    /*                             LCXmlLayoutBuilderBase:: */
-    /*                             mCommonLayoutsAttributes.layoutType)); */
+    //Создание элементов строки.
+    for( QDomNode node = _element.firstChild(); 
+            !node.isNull(); node = node.nextSibling())
+    {
+        if(!node.isElement()) continue;
 
-    /*             if(!builder.isNull()) */
-    /*             { */
-    /*                 layout = builder->build(nodeElement, _app); */
-    /*             } */
+        QDomElement el =  node.toElement();
 
-    /*             if(layout) */
-    /*             { */
-    /*                 _buildData.mpLayout-> */
-    /*                     addLayout(layout, row, _buildData.mColumn); */
-    /*             } */
-    /*             else */
-    /*             { */
-    /*                 _buildData.mpLayout-> */
-    /*                     addItem(new QSpacerItem(0,0), row, _buildData.mColumn); */
-    /*             } */
-    /*         } */
-    /*         else */
-    /*         { */
-    /*             QWidget* widget = nullptr; */
+        if(el.tagName() == __sTags.widgets)
+        {
+            curr_row += addColWidgets(el, _buildData, curr_row, _app);
+        }
+        else if(el.tagName() == __sTags.layout)
+        {
+            curr_row += addColLayout(el, _buildData, curr_row, _app);
+        }
+        else if(el.tagName() == __sTags.stretch)
+        {
+            curr_row += addColStretch(el, _buildData, curr_row);
+        }
+    }
 
-    /*             QSharedPointer<LIXmlWidgetBuilder> wb = */ 
-    /*                 _app.getWidgetBuilder(nodeElement.tagName()); */
+    if(curr_row > _buildData.mRow) _buildData.mRow = curr_row;
+    _buildData.mColumn++;
+}
 
-    /*             if(!wb.isNull()) */
-    /*             { */
-    /*                 widget = wb->build(nodeElement, _app); */
-    /*             } */
+//==============================================================================addColWidgets
+static int addColWidgets(
+        const QDomElement &_element,
+        CBuildData &_buildData, 
+        int _startRow, 
+        const LIApplication& _app)
+{
+    int row = 0;
 
-    /*             if(widget) */
-    /*             { */
-    /*                 _buildData.mpLayout-> */
-    /*                     addWidget(widget, row, _buildData.mColumn); */
-    /*             } */
-    /*             else */
-    /*             { */
-    /*                 _buildData.mpLayout-> */
-    /*                     addItem(new QSpacerItem(0,0), row, _buildData.mColumn); */
-    /*             } */
-    /*         } */
-    /*         row++; */
-    /*     } */
-    /*     //-------------------------------- */
-    /*     childNode = childNode.nextSibling(); */
-    /* } */
-    /* _buildData.mColumn++; */
-    /* if(row > _buildData.mRow) _buildData.mRow = row; */
+    quint16 align_flags = 0; 
+
+    //Анализ атрибута выравнивания элемента.
+    QString attr = _element.attribute(
+                    CCommonAttributes::instance().mAligns.attrName);
+    if(!attr.isNull())
+    {
+        //Если для элемента установлен атрибут выравнивания,
+        //производим его расшифровку.
+        align_flags = CCommonAttributes::instance().mAligns.toFlags(attr);
+    }
+    else if(_buildData.mColumnAlign.contains(_buildData.mColumn))
+    {
+        //Если для стобца установлено глобальное выравнивание,
+        //используем его.
+       align_flags = _buildData.mColumnAlign[_buildData.mColumn]; 
+    }
+
+    for( QDomNode node = _element.firstChild(); 
+            !node.isNull(); 
+            node = node.nextSibling() )
+    {
+        QDomElement el = node.toElement();
+        if(el.isNull()) continue;
+        auto builder = _app.getWidgetBuilder(el.tagName());
+        if(builder.isNull()) continue;
+        auto widget = builder->build(el, _app);
+        if(widget == nullptr) continue;
+
+        //Анализ параметров выравнивания для текущего стролбца.
+        if(align_flags == 0)
+        {
+            if(_buildData.mRowAlign.contains(row))
+            {
+                align_flags = _buildData.mRowAlign[row];
+            }
+            else
+            {
+                align_flags = Qt::AlignmentFlag::AlignLeft;
+            }
+        }
+
+        _buildData.mpLayout->addWidget( 
+                widget, 
+                row + _startRow,
+                _buildData.mColumn, 
+                static_cast<Qt::AlignmentFlag>(align_flags)); 
+        row++;
+    }
+    return row;
+}
+
+//==============================================================================addColLayout
+static int addColLayout(
+        const QDomElement &_element,
+        CBuildData &_buildData, 
+        int _startRow, 
+        const LIApplication& _app)
+{
+    for(    QDomNode node = _element.firstChild(); 
+            !node.isNull(); 
+            node = node.nextSibling())
+    {
+        if(!node.isElement()) continue;
+        QDomElement el = node.toElement();
+        auto builder = _app.getLayoutBuilder(el.tagName());
+        if(builder.isNull()) continue;
+        auto layout = builder->build(el, _app);
+        if(layout == nullptr) continue;
+        _buildData.mpLayout->addLayout(layout, _startRow, _buildData.mColumn);
+        return 1;
+    }
+    return 0; 
+}
+
+//==============================================================================addColStretch
+static int addColStretch(
+        const QDomElement &_element,
+        CBuildData &_buildData, 
+        int _startRow)
+{
+    QString attr_val = _element.attribute(__sAttributes.value);
+    bool flag = false;
+    int val = attr_val.toInt(&flag);
+
+    if(flag)
+    {
+        _buildData.mpLayout->setRowStretch(_startRow, val);
+    }
+    else
+    {
+        _buildData.mpLayout->setRowStretch(_startRow, 1);
+    }
+
+    return 0;
 }
