1ffbb0733fe9a921bb8da62dc2b41ad620728d75
SergeyKuzmenko <serkuzmo@gmail.com>
2020-10-19 21:22:33 +0300

    Первичная версия LCQRemWriteButton.

--- basicwidgets/widgets/lcqremwritebutton.h
+++ basicwidgets/widgets/lcqremwritebutton.h
@@ -10,20 +10,24 @@ private:
     class CWriteListener : public LIRemoteDataWriteListner 
     {
     public:
-        CWriteListener();
+        CWriteListener(){};
         ~CWriteListener(){};
-        virtual void dataIsWrite(LERemoteDataStatus _status) override {Q_UNUSED(_status);}
+        virtual void dataIsWrite(LERemoteDataStatus _status) override;
     };
+    QString mDataNameWrite;
     QByteArray mData;
+    QSharedPointer<LIRemoteDataWriter> mDataWriter;
+    QSharedPointer<LIRemoteDataWriteListner> mDataWriteListener;
 private:
     explicit LCQRemWriteButton(QPushButton* _parent = nullptr);
     explicit LCQRemWriteButton(const QString& _text, QPushButton* _parent = nullptr);
 public:
     explicit LCQRemWriteButton(
-                const QString& _dataNameWrite, 
-                const QByteArray _data, 
-                const QString& _text, 
-                QPushButton* _parent = nullptr);
+                QSharedPointer<LIRemoteDataSource> _dataSource,
+                const QString&      _dataNameWrite, 
+                const QByteArray&   _data, 
+                const QString&      _text, 
+                QPushButton*        _parent = nullptr);
 };
 
 #endif /* LCQREMWRITEBUTTON_H_ */
