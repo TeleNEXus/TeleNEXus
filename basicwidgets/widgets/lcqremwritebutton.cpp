e7cebce24f9d24b01998d710d376221ae722675f
SergeyKuzmenko <serkuzmo@gmail.com>
2020-11-18 22:24:03 +0300

    Востановлены файлы.

---
+++ basicwidgets/widgets/lcqremwritebutton.cpp
@@ -0,0 +1,69 @@
+#include "lcqremwritebutton.h"
+#include "LIRemoteDataWriter.h"
+#include <QDebug>
+
+//==============================================================================CWriteListener
+LCQRemWriteButton::CWriteListener::CWriteListener(
+        const QString& _dataName):
+    mDataName(_dataName)
+{
+}
+
+//------------------------------------------------------------------------------dataIsWrite
+void LCQRemWriteButton::CWriteListener::dataIsWrite(LERemoteDataStatus _status)
+{
+    Q_UNUSED(_status); 
+}
+
+//==============================================================================SWriteSet
+void LCQRemWriteButton::SWriteSet::write()
+{
+    mWriter->writeRequest(mData);
+}
+
+//==============================================================================LCQRemWriteButton
+LCQRemWriteButton::LCQRemWriteButton(QPushButton* _parent)
+{
+    LCQRemWriteButton("RemWriteButton", _parent);
+}
+
+LCQRemWriteButton::LCQRemWriteButton(
+                const QString&      _text, 
+                QPushButton*        _parent) : 
+    QPushButton(_text, _parent)
+{
+    QString text = _text;
+    if(text.isNull()) text = "RemWriteButton";
+    //Подключение обработки сигнала нажания.
+    connect(this, &LCQRemWriteButton::pressed, 
+            [&]()
+            {
+            for(    auto it = mListWriteSets.begin(); 
+                    it != mListWriteSets.end(); 
+                    it++)
+            {
+            it->write();
+            }
+            });
+}
+
+//------------------------------------------------------------------------------addDataWrite
+void LCQRemWriteButton::addDataWrite(
+        QSharedPointer<LIRemoteDataSource>  _source, 
+        const QString&                      _dataName, 
+        const QByteArray&                   _data)
+{
+    SWriteSet write_set;
+
+    if(_source.isNull()) return;
+
+    write_set.mListener = 
+        QSharedPointer<LIRemoteDataWriteListner>(new CWriteListener(_dataName));
+
+    write_set.mData = _data;
+    write_set.mWriter = _source->createWriter();
+    write_set.mWriter->setDataName(_dataName);
+    write_set.mWriter->setDataWriteListener(write_set.mListener);
+    write_set.mDataName = _dataName;
+    mListWriteSets << write_set;
+}
---
+++ basicwidgets/widgets/lcqremwritebutton.h
@@ -0,0 +1,44 @@
+#ifndef LCQREMWRITEBUTTON_H_
+#define LCQREMWRITEBUTTON_H_
+
+#include "LIRemoteDataSource.h"
+#include "LIRemoteDataWriteListner.h"
+#include <QPushButton>
+#include <QList>
+
+class LCQRemWriteButton : public QPushButton
+{
+private:
+    class CWriteListener : public LIRemoteDataWriteListner 
+    {
+    private:
+        QString mDataName;
+    public:
+        CWriteListener(){};
+        CWriteListener(const QString& _dataName);
+        ~CWriteListener(){};
+        virtual void dataIsWrite(LERemoteDataStatus _status) override;
+    };
+
+    struct SWriteSet
+    {
+        QByteArray mData;
+        QString mDataName;
+        QSharedPointer<LIRemoteDataWriter> mWriter;
+        QSharedPointer<LIRemoteDataWriteListner> mListener;
+        void write();
+    };
+
+    QList<SWriteSet> mListWriteSets;
+
+public:
+    explicit LCQRemWriteButton(QPushButton* _parent = nullptr);
+    explicit LCQRemWriteButton(const QString& _text, QPushButton* _parent = nullptr);
+
+    void addDataWrite(
+            QSharedPointer<LIRemoteDataSource>  _source, 
+            const QString&                      _dataName, 
+            const QByteArray&                   _data);
+};
+
+#endif /* LCQREMWRITEBUTTON_H_ */
