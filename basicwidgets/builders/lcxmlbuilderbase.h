962bf4b3fd6c382ddf580b8e122d5256ad5bb765
skzow <setproof@yandex.ru>
2020-11-25 23:42:11 +0300

    Начато изменение интерфейса футкции buildLocal.

--- basicwidgets/builders/lcxmlbuilderbase.h
+++ basicwidgets/builders/lcxmlbuilderbase.h
@@ -2,9 +2,19 @@
 #define LCXMLBUILDERBASE_H_
 
 #include "LIXmlWidgetBuilder.h"
+#include <QPoint>
+#include <QSharedPointer>
+#include <qglobal.h>
 
 class LCXmlBuilderBase : public LIXmlWidgetBuilder
 {
+protected:
+  struct SBaseData
+  {
+    QPoint pos;
+    const QDomElement& element;
+    const LIApplication& application;
+  };
 public:
 
   LCXmlBuilderBase();
@@ -19,6 +29,9 @@ protected:
   virtual QWidget* buildLocal(
       const QDomElement&      _element, 
       const LIApplication&    _app) = 0;
+
+  virtual QWidget* buildLocal1(QSharedPointer<SBaseData> _buildData)
+  {Q_UNUSED(_buildData); return nullptr;}
 };
 
 #endif
