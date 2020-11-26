3ba7002b86b87e5717b7d38fef035511b5346065
skzow <setproof@yandex.ru>
2020-11-26 20:03:57 +0300

    Корректировка форматтеров.

--- basicwidgets/dataformatters/lcstringdataformatterbits.h
+++ basicwidgets/dataformatters/lcstringdataformatterbits.h
@@ -10,14 +10,14 @@ private:
     class CValidator : public QValidator
     {
     public:
-        const int&      mSize;
-        const QChar&    mSeparator;
+        int      mSize;
+        QChar    mSeparator;
 
         explicit CValidator(
-                const int& _size, 
-                const QChar& _separator, 
+                int _size = 0, 
+                QChar _separator = QChar(), 
                 QObject *_parent = nullptr);
-        ~CValidator(){}
+        virtual ~CValidator(){}
 
         virtual QValidator::State validate(
                 QString &_input, 
@@ -25,11 +25,9 @@ private:
     };
 
 private:
-    int         mSize;            //Заданный размер последовательности в байтах.
-    QChar       mSeparator;       //Разделитель между отображаемыми числами.
     QChar       mFillCharUndef;   //Заполнение при отсутствии значения.
     QChar       mFillCharWrong;   //Заполнение при ошибочном значении.
-    CValidator* mpValidator;      //Валидатор.
+    CValidator  mValidator;      //Валидатор.
 public:
 
     explicit LCStringDataFormatterBits(    
@@ -49,38 +47,13 @@ public:
     virtual QString     normalizeString(const QString& _str) override;
     virtual QByteArray  toBytes(const QString& _str) override;
     virtual QString     undefStateString() override;
-    virtual QValidator* validator() override {return mpValidator;}
+    virtual QValidator* validator() override {return &mValidator;}
 
     void setFillCharUndef(QChar _c){mFillCharUndef = _c;}
     void setFillCharWrong(QChar _c){mFillCharWrong = _c;}
     void setSize(int _size);
     void setSeparator(QChar _separator);
 };
-
-
-
-/* class LCStringDataFormatterBits : public LCStringDataFormatterBase */
-/* { */
-/* private: */
-/*     int     mSize;              //Заданный размер последовательности в битах. */
-/*     QChar   mFillCharUndef;     //Заполнение при отсутствии значения. */
-/*     QChar   mFillCharWrong;     //Заполнение при ошибочном значении. */
-/* public: */
-/*     explicit LCStringDataFormatterBits( */    
-/*             int     _size           = -1, */ 
-/*             QChar   _fillCharUndef  = msFillCharUndefDef, */
-/*             QChar   _fillCharWrong  = msFillCharWrongDef); */
-/*     virtual ~LCStringDataFormatterBits(){} */
-/*     virtual QString     toString(const QByteArray& _data) override; */
-/*     virtual QString     normalizeString(const QString& _str) override; */
-/*     virtual QByteArray  toBytes(const QString& _str) override; */
-/*     virtual QString     undefStateString() override; */
-/*     virtual QValidator* validator() override {return nullptr;} */
-
-/*     void setFillCharUndef(QChar _c){mFillCharUndef = _c;} */
-/*     void setFillCharWrong(QChar _c){mFillCharWrong = _c;} */
-/*     void setSize(int _size){mSize = _size;} */
-/* }; */
 
 #endif // LCSTRINGDATAFORMATTERBITS_H
 
