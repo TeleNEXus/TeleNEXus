f63029500e6f2996c1d27d0fe45470c0a8d97ddb
skzow <setproof@yandex.ru>
2020-12-11 08:58:44 +0300

    Добавлено управление состоянием видимости LCQRemLabel при налиции и отсутсвии связи.

--- basicwidgets/widgets/lcqremlabel.cpp
+++ basicwidgets/widgets/lcqremlabel.cpp
@@ -4,48 +4,57 @@
 #include <qnamespace.h>
 
 //==============================================================================LCQRemLabel
-LCQRemLabel::CReadListener::CReadListener(LCQRemLabel& _label) : mLabel(_label)
+LCQRemLabel::CReadListener::CReadListener(LCQRemLabel& _label) : 
+  mLabel(_label),
+  mFlagActive(false)
 {
 
 }
 
 void LCQRemLabel::CReadListener::dataIsRead(QSharedPointer<QByteArray>  _data, 
-                                            LERemoteDataStatus          _status)
+    LERemoteDataStatus          _status)
 {
-  if(_status != LERemoteDataStatus::DS_OK)
+  if(mFlagActive)
   {
-    QString str = "Undef";
-    mLabel.mFormatter.data()->undefState(str);
+    if(_status != LERemoteDataStatus::DS_OK)
+    {
+      QString str = "Undef";
+      mLabel.mFormatter.data()->undefState(str);
+      mLabel.setText(str);
+      mLabel.setEnabled(false);
+      return;
+    }
+    QString str = mLabel.mFormatter.data()->toString(*_data);
     mLabel.setText(str);
-    return;
+    mLabel.setEnabled(true);
   }
-  QString str = mLabel.mFormatter.data()->toString(*_data);
-  mLabel.setText(str);
 }
 
 //==============================================================================LCQRemLabel
 LCQRemLabel::LCQRemLabel(QWidget* _parent) : QLabel(_parent)
 {
-    setText("LCQRemLabel");
+  setText("LCQRemLabel");
+  setEnabled(false);
 }
 
 LCQRemLabel::LCQRemLabel(QString _text, QWidget* _parent) : QLabel(_text, _parent)
 {
+  setEnabled(false);
 }
 
 LCQRemLabel::LCQRemLabel(const QString& _dataName,
-                         QSharedPointer<LIRemoteDataSource> _dataSource,
-                         QSharedPointer<LIDataFormatter> _formatter,
-                         QWidget* _parent) :    QLabel(_parent),
-                                                mDataName(_dataName),
-                                                mFormatter(_formatter)
+    QSharedPointer<LIRemoteDataSource> _dataSource,
+    QSharedPointer<LIDataFormatter> _formatter,
+    QWidget* _parent) :    QLabel(_parent),
+  mDataName(_dataName),
+  mFormatter(_formatter)
 {
-    QString str = "Undef";
-    mFormatter.data()->undefState(str);
-    setText(str);
-    mDataListener = QSharedPointer<CReadListener>(new CReadListener(*this));
-    mDataReader = _dataSource->createReader(_dataName, mDataListener);
-
+  setEnabled(false);
+  QString str = "Undef";
+  mFormatter.data()->undefState(str);
+  setText(str);
+  mDataListener = QSharedPointer<CReadListener>(new CReadListener(*this));
+  mDataReader = _dataSource->createReader(_dataName, mDataListener);
 }
 
 LCQRemLabel::~LCQRemLabel()
@@ -55,34 +64,38 @@ LCQRemLabel::~LCQRemLabel()
 //------------------------------------------------------------------------------setActive
 void LCQRemLabel::setActive(bool _flag)
 {
-    if(_flag)
-    {
-        mDataReader->connectToSource();
-    }
-    else
-    {
-        mDataReader->disconnectFromSource();
-    }
+  if(_flag)
+  {
+    mDataListener->setActive(true);
+    mDataReader->connectToSource();
+  }
+  else
+  {
+    mDataListener->setActive(false);
+    mDataReader->disconnectFromSource();
+  }
 }
 
 //------------------------------------------------------------------------------event
 bool LCQRemLabel::event(QEvent *_event)
 {
-    bool ret = false;
-    switch(_event->type())
-    {
-    case QEvent::Type::Show:
-        mDataReader->connectToSource();
-        ret = true;
-        break;
-    case QEvent::Type::Hide:
-        mDataReader->disconnectFromSource();
-        ret = true;
-        break;
-    default:
-        break;
-    }
-    QLabel::event(_event);
-    return ret;
+  bool ret = false;
+  switch(_event->type())
+  {
+  case QEvent::Type::Show:
+    /* mDataReader->connectToSource(); */
+    setActive(true);
+    ret = true;
+    break;
+  case QEvent::Type::Hide:
+    setActive(false);
+    /* mDataReader->disconnectFromSource(); */
+    ret = true;
+    break;
+  default:
+    break;
+  }
+  QLabel::event(_event);
+  return ret;
 }
 
