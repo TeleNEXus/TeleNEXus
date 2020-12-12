b0a138095f475aedcc213b4a17b85579ad504935
SergeyKuzmenko <serkuzmo@gmail.com>
2020-12-12 14:31:38 +0300

    Дабавлены файлы форматтера строкового типа.

---
+++ basicwidgets/dataformatters/lcformatterstring.h
@@ -0,0 +1,18 @@
+﻿#ifndef LCFORMATTERSTRING_H
+#define LCFORMATTERSTRING_H
+
+#include "lcformatterbase.h"
+#include <QValidator>
+
+class LCFormatterString : public LCFormatterBase
+{
+public:
+  explicit LCFormatterString();
+
+  virtual QString     toString(const QByteArray& _data) override;
+  virtual QString     normalize(const QString& _str) override;
+  virtual QByteArray  toBytes(const QString& _str) override;
+  virtual QValidator* validator() override;
+};
+
+#endif // LCFORMATTERSTRING_H
