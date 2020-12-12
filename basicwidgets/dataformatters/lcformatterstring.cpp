184d7b11818c2f157abfac1ebc5065fff1b10922
skzow <setproof@yandex.ru>
2020-12-12 14:31:38 +0300

    Дабавлены файлы форматтера строкового типа.

---
+++ basicwidgets/dataformatters/lcformatterstring.cpp
@@ -0,0 +1,33 @@
+﻿#include "lcformatterstring.h"
+
+//==============================================================================LCQDataFormatterUint8
+LCFormatterString::LCFormatterString() 
+{
+}
+
+//------------------------------------------------------------------------------toString
+QString LCFormatterString::toString(const QByteArray& _data)
+{
+  return QString(_data);
+}
+
+//------------------------------------------------------------------------------normalize
+QString LCFormatterString::normalize(const QString& _str)
+{
+  return _str;
+}
+
+//------------------------------------------------------------------------------toBytes
+QByteArray LCFormatterString::toBytes(const QString& _str)
+{
+  return _str.toLatin1();
+}
+
+
+//------------------------------------------------------------------------------validator
+QValidator* LCFormatterString::validator()
+{
+  return nullptr;
+}
+
+
