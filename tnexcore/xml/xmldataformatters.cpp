80486e1a9160444db190c07edaa453e6a55d7f5c
SergeyKuzmenko <serkuzmo@gmail.com>
2020-12-17 10:32:29 +0300

    Добавлены файлы для доступа к форматтером данных.

---
+++ tnexcore/xml/xmldataformatters.cpp
@@ -0,0 +1,102 @@
+/* 
+ * TeleNEXus is a simple SCADA programm
+ *
+ * Copyright (C) 2020 Sergey S. Kuzmenko
+ *
+ * This file is part of TeleNEXus.
+ *
+ * TeleNEXus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TeleNEXus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TeleNEXus.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include <xmldataformatters.h>
+#include "LIApplication.h"
+#include "LIDataFormatter.h"
+
+#include <functional>
+#include <QDomElement>
+#include <QMap>
+
+static const struct
+{
+  QString id        = "id";
+  QString stdformat = "stdformat";
+  QString script    = "script";
+}__slAttributes;
+
+static const struct
+{
+  QString add = "add";
+}__slTags;
+
+static class CCreator
+{
+private:
+  QMap<QString, std::function<void(const QDomElement&)>> mCreatorsMap;
+public:
+  //----------------------------------------------------------------------------
+  CCreator()
+  {
+    mCreatorsMap.insert(__slAttributes.stdformat,
+        [](const QDomElement& _element)
+        {
+          Q_UNUSED(_element);
+        });
+
+    mCreatorsMap.insert(__slAttributes.script,
+        [](const QDomElement& _element)
+        {
+          Q_UNUSED(_element);
+        });
+  }
+
+  //----------------------------------------------------------------------------
+  void create(const QDomElement& _element)
+  {
+    QString attr = _element.attribute(__slAttributes.id);
+    if(attr.isNull()) return;
+    auto it = mCreatorsMap.find(attr);
+    if(it == mCreatorsMap.end()) return;
+    it.value()(_element);
+  }
+
+}__slCreator;
+
+namespace xmlformatters
+{
+
+static QMap<QString, QSharedPointer<LIDataFormatter>> __slFormattersMap;
+
+//------------------------------------------------------------------------------
+void create( const QDomElement &_element, const LIApplication& _app)
+{
+  Q_UNUSED(_app);
+
+  for(auto node = _element.firstChildElement(__slTags.add);
+      !node.isNull();
+      node = node.nextSiblingElement(__slTags.add))
+  {
+    __slCreator.create(node.toElement());
+  }
+}
+
+//------------------------------------------------------------------------------
+QSharedPointer<LIDataFormatter> getDataFormatter(const QString& _formatterId)
+{
+  auto it = __slFormattersMap.find(_formatterId);
+  if(it == __slFormattersMap.end()) return nullptr;
+  return it.value();
+}
+
+}
+
+
