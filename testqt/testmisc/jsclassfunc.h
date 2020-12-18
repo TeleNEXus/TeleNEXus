912eceb18daadf7bbe1686890c0f93827d61cd9a
SergeyKuzmenko <serkuzmo@gmail.com>
2020-12-18 16:25:33 +0300

    Тестирование валидаторов.

--- testqt/testmisc/jsclassfunc.h
+++ testqt/testmisc/jsclassfunc.h
@@ -4,16 +4,49 @@
 #include <QVariant>
 #include <QVector>
 #include <QVariantList>
+
+enum class EValidateState
+{
+  intermediate,
+  acceptable,
+  invalid
+};
+
+/* class LQValidateStatus : public QObject */
+/* { */
+/*   Q_OBJECT; */
+/* public: */
+/*   Q_PROPERTY(EValidateState intermediate READ getIntermediate SCRIPTABLE true); */ 
+/*   Q_PROPERTY(EValidateState acceptable READ getAcceptable SCRIPTABLE true); */ 
+/*   Q_PROPERTY(EValidateState invalid READ getInvalid SCRIPTABLE true); */ 
+
+/* public: */
+/*   explicit LQValidateStatus(): QObject(nullptr) {} */
+/*   EValidateState getIntermediate() { return (EValidateState::intermediate);} */
+/*   EValidateState getAcceptable() { return   (EValidateState::acceptable);} */
+/*   EValidateState getInvalid() { return      (EValidateState::invalid);} */
+/* }; */
+
+
 class LJsClass : public QObject
 {
-  Q_OBJECT
+  Q_OBJECT;
+  Q_PROPERTY(EValidateState Intermediate  READ getIntermediate SCRIPTABLE true); 
+  Q_PROPERTY(EValidateState Acceptable    READ getAcceptable SCRIPTABLE true); 
+  Q_PROPERTY(EValidateState Invalid       READ getInvalid SCRIPTABLE true); 
 public:
-    explicit LJsClass(QObject* _parent = nullptr);
-    virtual ~LJsClass();
+  explicit LJsClass(QObject* _parent = nullptr);
+  virtual ~LJsClass();
 
-public slots:
+  EValidateState getIntermediate()  { return EValidateState::intermediate;}
+  EValidateState getAcceptable()    { return EValidateState::acceptable;}
+  EValidateState getInvalid()       { return EValidateState::invalid;}
+
+  public slots:
   void debug(const QString&);
-    
 };
+
+Q_DECLARE_METATYPE(EValidateState);
+
 #endif
 
--- testqt/testmisc/testjsfunc.js
+++ testqt/testmisc/testjsfunc.js
@@ -1,7 +1,11 @@
 var counter = 0;
+var min = -100;
+var max = 100;
+
 function validate(val)
 {
   if(typeof val != "string") {return false;}
+
   var data = parseInt(val);
   if(isNaN(data))
   {
@@ -12,3 +16,9 @@ function validate(val)
 }
 
 Application.debug("Jave Script Evaluate");
+// Application.debug("Jave Script Evaluate EValidateStatus intermediate = " + EValidateStatus.intermediate);
+// Application.debug("Jave Script Evaluate EValidateStatus acceptable = " + EValidateStatus.acceptable);
+// Application.debug("Jave Script Evaluate EValidateStatus invalid = " + EValidateStatus.invalid);
+Application.debug("Jave Script Evaluate EValidateStatus intermediate = " + Application.Intermediate);
+Application.debug("Jave Script Evaluate EValidateStatus acceptable = " + Application.Acceptable);
+Application.debug("Jave Script Evaluate EValidateStatus invalid = " + Application.Invalid);
