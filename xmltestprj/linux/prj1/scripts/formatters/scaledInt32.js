e41e56ed6d6d967d52cada2a6f766ab890e14b02
SergeyKuzmenko <serkuzmo@gmail.com>
2020-12-25 16:13:27 +0300

    Произведена первичная отладка форматтера данных, не реализовани функция подгонки строки.

--- xmltestprj/linux/prj1/scripts/formatters/scaledInt32.js
+++ xmltestprj/linux/prj1/scripts/formatters/scaledInt32.js
@@ -13,17 +13,18 @@ for(var i = 0; i < attributes_keys.length; i++) {
 }
 
 function initValrange(){
-  var minval = -2147483648;
-  var maxval = 2147483647;
-  var buff = parseInt(Attributes.min);
+  var min = -2147483648;
+  var max = 2147483647;
+
+  var buff = parseFloat(Attributes.min);
   if(!isNaN(buff)){
-    minval = buff;
+    min = buff;
   }
-  buff = parseInt(Attributes.max);
+  buff = parseFloat(Attributes.max);
   if(!isNaN(buff)){
-    maxval = buff;
+    max = buff;
   }
-  return {min : minval, max : maxval};
+  return {min : min, max : max};
 }
 
 var valrange = initValrange();
@@ -31,28 +32,45 @@ var valrange = initValrange();
 DebugOut("valrange.min = " + valrange.min);
 DebugOut("valrange.max = " + valrange.max);
 
-function initScale(){
+function initFormat(){
+
+  var format = {
+    intsize   : 0,
+    fractsize : 0,
+    scale     : 1,
+    pow       : 0
+  }
+
   if(Attributes.format === undefined) {
-    DebugOut("scaledInt32 format is undefined");
-  }
-  else {
-    DebugOut("scaledInt32 format = " + Attributes.format);
+    return format;
   }
 
-  // re = new RegExp("x*");
-  var re = /\.x*/;
-  var regExp = new RegExp("(\..+)$");
+  var regExp = /\..+$/;
 
-  if(!regExp.test(Attributes.format)) {
-    DebugOut("scaledInt32 wrong format string.");
+  format.fractsize = Attributes.format.search(regExp);
+
+  if(format.fractsize < 0) {
+    format.fractsize = 0;
   }
   else {
-    DebugOut("scaledInt32 format string is correct.");
+    format.fractsize = Attributes.format.length - 1 - format.fractsize;
+    if(format.fractsize < 0) format.fractsize = 0;
   }
-  return 0
+
+  format.intsize = Attributes.format.length - 1 - format.fractsize;
+  if(format.intsize < 0) format.intsize = 0;
+
+  format.scale = Math.pow(10, (-1)*format.fractsize);
+  format.pow = format.fractsize;
+
+  DebugOut("scaledInt32 intsize = " + format.intsize);
+  DebugOut("scaledInt32 fractsize = " + format.fractsize);
+  DebugOut("scaledInt32 scale = " + format.scale);
+
+  return format;
 }
 
-var scale = initScale();
+var gvFormat = initFormat();
 
 //------------------------------------------------------------------------------Validate
 function Validate(_val){
@@ -65,9 +83,17 @@ function Validate(_val){
 
   if(_val.length === 0) return Intermediate;
 
-  if(_val === "-") return Intermediate;
+  if((_val === "-") || (_val === ".")) return Intermediate;
 
-  var data = parseInt(_val);
+  if(gvFormat.scale === 1) {
+    if(!RegExp(/^-{0,1}[0-9]*$/).test(_val)) return Invalid;
+    var data = parseInt(_val);
+  }
+  else {
+    DebugOut("gvFormat.scale = " + gvFormat.scale);
+    if(!RegExp(/^-{0,1}([0-9]*)(\.{0,1})([0-9]*$)/).test(_val)) return Invalid;
+    var data = parseFloat(_val);
+  }
 
   if(isNaN(data)) {
     DebugOut("Validate data is NaN!");
@@ -90,26 +116,64 @@ function arrayToInt(_data) {
   return res;
 }
 
+function formatting(_input){
+
+  var str = _input.toFixed(gvFormat.fractsize);
+
+  var len = gvFormat.intsize + gvFormat.fractsize + 1;
+
+  var minus = "";
+
+  if(str.charAt(0) == "-") {
+    minus = "-";
+  }
+
+  str = str.replace(/(^-){1,1}/,"");
+  if(str.length < len)
+  {
+    var addzero = "";
+    for (var i = 0; i < (len - str.length); i++)
+    {
+      addzero += "0";
+    }
+    str = addzero + str;
+  }
+  
+  return minus + str;
+}
+
 //------------------------------------------------------------------------------ToString
 function ToString(_data) {
 
   DebugOut("ToString data = " + _data);
+
   var res_int = arrayToInt(_data);
+
   DebugOut("arrayToInt() = " + res_int);
-  return res_int.toString();
+
+  res_int *= gvFormat.scale;
+
+  return formatting(res_int);
+}
+
+function parseToNum(_str)
+{
+  if(gvFormat.scale === 1) return parseInt(_str);
+  return parseFloat(_str) * Math.pow(10, gvFormat.pow);
 }
 
 //------------------------------------------------------------------------------ToBytes
 function ToBytes(_str) {
 
   var array = [0,0,0,0];
-  var int_data = parseInt(_str);
+  var num_data = parseToNum(_str);
+
   DebugOut("======================================================ToBytes");
-  DebugOut("int_data = " + int_data);
-  if(isNaN(int_data)) return [];
+  DebugOut("int_data = " + num_data);
+  if(isNaN(num_data)) return [];
   for (var i = 0; i < 4; i++)
   {
-    array[i] = 0x000000ff & (int_data >>> (8*i));
+    array[i] = 0x000000ff & (num_data >>> (8*i));
   }
   DebugOut("------------------------------ToBytes" + array);
   return array;
