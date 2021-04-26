4205768158e42bd6dcc99734700a1c00380066a7
SergeyKuzmenko <serkuzmo@gmail.com>
2021-04-26 15:30:18 +0300

    Изменен и отлажен механизм импорта модулей js.

--- xmltestprj/linux/prj1/scripts/testexportmodule1.js
+++ xmltestprj/linux/prj1/scripts/testexportmodule1.js
@@ -1,14 +1,19 @@
-(function (){
-  DebugOut("+++++Start Import Module");
-})();
+( function ()
+  {
+    console.debug(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Start Import Module");
+  }
+)();
 
-
-var importObject = { field1 : "field 1 meaning", field2 : "field 2 meaning"};
-
-function testImportDebug(debug_str){
+var localObject = { field1 : "local 1 meaning", field2 : "local 2 meaning"};
+export var importObject = { field1 : "field 1 meaning", field2 : "field 2 meaning"};
+export function testImportDebug(debug_str)
+{
   DebugOut(debug_str);
+  DebugOut("localObject: field 1 = " + localObject.field1 + "; field 2 = " + localObject.field2 + ";");
 }
 
-(function (){
-  console.debug("+++++End Import Module");
-})();
+( function ()
+  {
+    console.debug("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<End Import Module");
+  }
+)();
