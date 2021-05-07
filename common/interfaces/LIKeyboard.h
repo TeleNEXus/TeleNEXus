5de544bc767823e45884eda4b487266bda98286d
SergeyKuzmenko <serkuzmo@gmail.com>
2021-05-07 16:17:24 +0300

    Добавлен файл интерфейса клавиатуры.

---
+++ common/interfaces/LIKeyboard.h
@@ -0,0 +1,37 @@
+/* 
+ * TeleNEXus is a simple SCADA programm
+ *
+ * Copyright (C) 2020 Sergey S. Kuzmenko
+ *
+ * This file is part of TeleNEXus.
+ *
+ * TeleNEXus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TeleNEXus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TeleNEXus.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#ifndef LIKEYBOARD_H_ 
+#define LIKEYBOARD_H_
+
+#include <QSharedPointer>
+#include "LIRemoteDataSource.h"
+
+class LIKeyboard
+{
+public:
+    LIKeyboard(){}
+    virtual ~LIKeyboard(){}
+    virtual QSharedPointer<LIRemoteDataSource> getStreamSource() = 0;
+    virtual QString getStreamName() = 0;
+};
+
+#endif // LIKEYBOARD_H_
+
