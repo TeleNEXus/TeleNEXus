97c6653e2c430a64570b5b0a16896fef0b4486f6
skzow <setproof@yandex.ru>
2021-05-12 09:17:35 +0300

    Добавлены файлы.

---
+++ common/interfaces/keyboard/LIKeyboardListener.h
@@ -0,0 +1,37 @@
+/* 
+ * TeleNEXus is a simple SCADA programm
+ *
+ * Copyright (C) 2020 Sergey S. Kuzmenko
+ *
+ * This file is part of TeleNEXus.
+ *
+ * TeleNEXus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TeleNEXus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TeleNEXus.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIKEYBOARDLISTENER_H_
+#define LIKEYBOARDLISTENER_H_
+
+class QString;
+
+class LIKeyboardListener
+{
+public:
+  LIKeyboardListener(){};
+  virtual ~LIKeyboardListener(){};
+  virtual void change(const QString& _data) = 0;
+  virtual void enter() = 0;
+
+};
+
+#endif /*LIKEYBOARDLISTENER_H_*/
