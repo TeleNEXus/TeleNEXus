22d6206b6f6530a3d0012d8de544f8b2aa02c2ce
skzow <setproof@yandex.ru>
2021-05-17 16:20:04 +0300

    Проработать состояние окон.

--- xmltestprj/linux/prj1/scripts/windoweventscripts/script_2.js
+++ xmltestprj/linux/prj1/scripts/windoweventscripts/script_2.js
@@ -1,2 +1,6 @@
 console.debug("<<<<<<<<<<<<<<<< Begin evaluate window event script 2");
+function Main()
+{
+  console.debug("++++++ Main script 2");
+}
 console.debug(">>>>>>>>>>>>>>>> End evaluate window event script 2");
