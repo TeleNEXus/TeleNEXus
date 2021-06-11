a396a10fa53968668dff8fc6f5a7619230b7d442
skzow <setproof@yandex.ru>
2021-06-11 09:42:20 +0300

    Added global style sheets control.

---
+++ tnexcore/xml/xmlglobalstyle.cpp
@@ -0,0 +1,58 @@
+/* 
+ * TeleNEXus is a simple SCADA programm
+ *
+ * Copyright (C) 2020 Sergey S. Kuzmenko
+ *
+ * This file is part of TeleNEXus.
+ *
+ * TeleNEXus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TeleNEXus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TeleNEXus.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "xmlglobalstyle.h"
+#include "applicationinterface.h"
+
+#include <QDomElement>
+#include <QTextStream>
+#include <QFile>
+#include <QApplication>
+#include <QDebug>
+
+static const struct
+{
+  QString file = "file";
+}__slAttributes;
+
+static const struct
+{
+  QString rootTag = "GLOBALSTYLE"; 
+}__slTags; 
+
+namespace xmlglobalstyle
+{
+
+void upload(const QDomElement& _rootElement, QApplication& _qapp)
+{
+  auto element = _rootElement.firstChildElement(__slTags.rootTag);
+  if(element.isNull()) return;
+
+  QString attr_file = element.attribute( __slAttributes.file);
+  if(attr_file.isNull()) return;
+  QFile style_file(attr_file); 
+  if(style_file.open(QFile::OpenModeFlag::ReadOnly))
+  {
+    _qapp.setStyleSheet(QLatin1String(style_file.readAll()));
+  }
+}
+
+}
