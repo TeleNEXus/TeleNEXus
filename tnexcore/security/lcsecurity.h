6df611a4d23236a90457db7cb590b7eee50a8f24
skzow <setproof@yandex.ru>
2021-11-28 22:35:17 +0300

    Save.

---
+++ tnexcore/security/lcsecurity.h
@@ -0,0 +1,61 @@
+/* 
+ * TeleNEXus is a simple SCADA programm
+ *
+ * Copyright (C) 2020 Sergey S. Kuzmenko
+ *
+ * This file is part of TeleNEXus.
+ *
+ * TeleNEXus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TeleNEXus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TeleNEXus.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef LCSECURITY_H_
+#define LCSECURITY_H_
+
+#include <QObject>
+#include <QSet>
+#include <QEvent>
+#include <QPair>
+#include <QMap>
+
+class QDomElement;
+class LIApplication;
+class QTimer;
+
+class LCSecurity
+{
+private:
+
+  bool mFlagInit;
+  int mCurrentLevel;
+  QMap<QString, QPair<int, QByteArray>> mAccess;
+  QTimer* mpTimer;
+
+private:
+  LCSecurity();
+  LCSecurity(const LCSecurity&) = delete;
+  LCSecurity& operator=(const LCSecurity&) = delete;
+  ~LCSecurity();
+
+public:
+  static LCSecurity& instance();
+  void init(const QDomElement& _element, const LIApplication& _app);
+  QObject* createEventFilter(const QDomElement& _element, const QSet<QEvent::Type>& _events) const;
+
+  bool checkAccessLevel(const QString& _accessId);
+
+private:
+  void autorize(const QString& _accessId, const QString& _password);
+};
+
+#endif
---
+++ tnexcore/security/lqsecurityfilter.cpp
@@ -0,0 +1,37 @@
+/* 
+ * TeleNEXus is a simple SCADA programm
+ *
+ * Copyright (C) 2020 Sergey S. Kuzmenko
+ *
+ * This file is part of TeleNEXus.
+ *
+ * TeleNEXus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TeleNEXus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TeleNEXus.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "lqsecurityfilter.h"
+
+//==============================================================================
+LQSecurityFilter::LQSecurityFilter(THandler _handler) : 
+  QObject(nullptr)
+  ,mHandler(_handler)
+{
+}
+
+//------------------------------------------------------------------------------
+bool LQSecurityFilter::eventFilter(QObject* _obj, QEvent* _event)
+{
+  if((bool)mHandler) 
+    return mHandler(_obj, _event);
+  return false;
+}
