3b4f396d112f2c9877f953d4a6d86e130e4d2c3f
skzowin <setproof@yandex.ru>
2021-12-08 11:20:50 +0300

    Changed windows deploy script.

--- win_deploy.sh
+++ win_deploy.sh
@@ -3,14 +3,13 @@
 qmake=qmake.exe
 make=mingw32-make.exe
 
-
-v_win_launcher="`pwd`/win_launcher/__builds/release/launcher.exe"
+v_launcher_exe="`pwd`/tools/launcher/__builds/release/launcher.exe"
+v_launcher_lang_ru="`pwd`/tools/launcher/main_ru.qm"
+v_launcher_lang_en="`pwd`/tools/launcher/main_en.qm"
 v_modbussource="`pwd`/plugins/modbussource/__builds/release/modbussource.dll"
 v_tnex="`pwd`/tnexcore/__builds/release/tnex.exe"
 
-
 v_deploy_path="`pwd`/win_deploy/"
-
 
 v_offset_line="================================================"
 
@@ -36,7 +35,6 @@ deleteDir ()
     fi
 }
 
-
 if [ "$1" == "--clear" ]
 then
     echo "$v_offset_line Clear win deploy"
@@ -46,21 +44,16 @@ fi
 
 createDir "$v_deploy_path"
 
+# if ! cp $v_tnex  $v_deploy_path/
+# then
+#   exit 0
+# fi 
 
-if ! cp $v_tnex  $v_deploy_path/
-then
-  exit 0
-fi 
-
-if ! cp $v_modbussource $v_deploy_path/
-then
-  exit 0
-fi 
-
-if ! cp $v_win_launcher $v_deploy_path/
-then
-  exit 0
-fi 
+cp $v_tnex  $v_deploy_path/
+cp $v_modbussource $v_deploy_path/
+cp $v_launcher_exe $v_deploy_path/
+cp $v_launcher_lang_ru $v_deploy_path/
+cp $v_launcher_lang_en $v_deploy_path/
 
 cd "$v_deploy_path"
 
