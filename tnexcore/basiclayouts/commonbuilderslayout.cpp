059a02cd1dcd1cf1d1c240a947ac3607500b00d4
SergeyKuzmenko <serkuzmo@gmail.com>
2023-02-13 18:01:35 +0300

    Changed deprecated contstructor QFlag.

--- tnexcore/basiclayouts/commonbuilderslayout.cpp
+++ tnexcore/basiclayouts/commonbuilderslayout.cpp
@@ -32,53 +32,55 @@ CCommonAttributes::CCommonAttributes()
 Qt::Alignment
 CCommonAttributes::SAligns::toFlags(const QString& _attributes) const
 {
-  Qt::Alignment flags = 0;
 
-    if(_attributes.isNull())
-    {
-        return Qt::AlignLeft;
-    }
+  /* QFlag flags(0); */
+  Qt::Alignment flags;
 
-    QString alines = _attributes.toLower();
+  if(_attributes.isNull())
+  {
+    return Qt::AlignLeft;
+  }
 
-    if(alines.contains(vals.Left))
-    {
-        flags |= Qt::AlignLeft;
-    }
+  QString alines = _attributes.toLower();
 
-    if(alines.contains(vals.Right))
-    {
-        flags |= Qt::AlignRight;
-    }
+  if(alines.contains(vals.Left))
+  {
+    flags |= Qt::AlignLeft;
+  }
 
-    if((alines.contains(vals.Center)) && 
-        (!alines.contains(vals.HCenter)) &&
-        (!alines.contains(vals.VCenter)))
-    {
-        flags |= Qt::AlignCenter;
-    }
-    
-    if(alines.contains(vals.HCenter))
-    {
-        flags |= Qt::AlignHCenter;
-    }
+  if(alines.contains(vals.Right))
+  {
+    flags |= Qt::AlignRight;
+  }
 
-    if(alines.contains(vals.Top))
-    {
-        flags |= Qt::AlignTop;
-    }
+  if((alines.contains(vals.Center)) && 
+      (!alines.contains(vals.HCenter)) &&
+      (!alines.contains(vals.VCenter)))
+  {
+    flags |= Qt::AlignCenter;
+  }
 
-    if(alines.contains(vals.Bottom))
-    {
-        flags |= Qt::AlignBottom;
-    }
+  if(alines.contains(vals.HCenter))
+  {
+    flags |= Qt::AlignHCenter;
+  }
 
-    if(alines.contains(vals.VCenter))
-    {
-        flags |= Qt::AlignVCenter;
-    }
+  if(alines.contains(vals.Top))
+  {
+    flags |= Qt::AlignTop;
+  }
 
-    if(flags == 0) return Qt::AlignLeft;
+  if(alines.contains(vals.Bottom))
+  {
+    flags |= Qt::AlignBottom;
+  }
 
-    return flags;
+  if(alines.contains(vals.VCenter))
+  {
+    flags |= Qt::AlignVCenter;
+  }
+
+  if(flags == 0) return Qt::AlignLeft;
+
+  return flags;
 }
